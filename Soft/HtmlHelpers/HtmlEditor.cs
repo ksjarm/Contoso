96fc24bf397f7e4d57ee263ba0d594ff6f9c356d
Ksenija Jarmuhhamedova <ksjarm@ttu.ee>
2023-03-24 23:15:44 +0200

    Creating HtmlHelpers

---
+++ Soft/HtmlHelpers/HtmlEditor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq.Expressions;
+
+namespace Contoso.Soft.HtmlHelpers
+{
+	public static class HtmlEditor
+	{
+		public static IHtmlContent MyEditorFor<TModel, TResult>
+				(this IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e)
+		{
+			var s = htmlStrings(h, e);
+			return new HtmlContentBuilder(s);
+		}
+
+		internal static List<object> htmlStrings<TModel, TResult>(IHtmlHelper<TModel> h,
+			Expression<Func<TModel, TResult>> e) => new() {
+			new HtmlString("<dt class=\"col-sm-2\">"),
+			h.LabelFor(e),
+			new HtmlString("/dt>"),
+			new HtmlString("<dt class=\"col-sm-10\">"),
+			h.EditorFor(e),
+			h.ValidationMessageFor(e, "", new { @class = "text-danger"}),
+			new HtmlString("/dt>")
+			};
+	}
+}
---
+++ Soft/HtmlHelpers/HtmlViewer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq.Expressions;
+
+namespace Contoso.Soft.HtmlHelpers;
+	public static class HtmlViewer {
+		public static IHtmlContent ViewerFor<TModel, TResult>
+			(this IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e) {
+			var s = htmlStrings(h, e);
+			return new HtmlContentBuilder(s);
+		}
+
+	private static List<object> htmlStrings<TModel, TResult>(IHtmlHelper<TModel> h, 
+		Expression<Func<TModel, TResult>> e) => new () {
+			new HtmlString("<dt class=\"col-sm-2\">"),
+			h.DisplayNameFor(e),
+			new HtmlString("/dt>"),
+			new HtmlString("<dt class=\"col-sm-10\">"),
+			h.DisplayFor(e),
+			new HtmlString("/dt>")
+		};
+}
