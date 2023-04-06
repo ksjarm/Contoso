a0a7fefb78f91e00574ce7a61388176dff09ebb0
Ksenija Jarmuhhamedova <ksjarm@ttu.ee>
2023-04-06 17:42:46 +0300

    Cleaning views

--- Pages/HtmlHelpers/HtmlSelectItem.cs
+++ Pages/HtmlHelpers/HtmlSelectItem.cs
@@ -7,22 +7,20 @@ namespace Contoso.Pages.HtmlHelpers;
 public static class HtmlSelectItem
 {
     public static IHtmlContent SelectItem<TModel, TValue>(this IHtmlHelper<TModel> h,
-        Expression<Func<TModel, TValue>> value,
-        IEnumerable<SelectListItem> items) {
+        Expression<Func<TModel, TValue>> value, IEnumerable<SelectListItem> items) {
         var s = htmlStrings(h, value, items, value);
         return new HtmlContentBuilder(s);
     }
     public static IHtmlContent SelectItem<TModel, TValue, TLabel>(this IHtmlHelper<TModel> h,
-        Expression<Func<TModel, TValue>> value,
-        IEnumerable<SelectListItem> items,
+        Expression<Func<TModel, TValue>> value, IEnumerable<SelectListItem> items,
         Expression<Func<TModel, TLabel>> label) {
         var s = htmlStrings(h, value, items, label);
         return new HtmlContentBuilder(s);
     }
     internal static List<object> htmlStrings<TModel, TValue, TLabel>(IHtmlHelper<TModel> h,
-        Expression<Func<TModel, TValue>> value,
-        IEnumerable<SelectListItem> items,
-        Expression<Func<TModel, TLabel>> label) => new() {
+         Expression<Func<TModel, TValue>> value,
+         IEnumerable<SelectListItem> items,
+         Expression<Func<TModel, TLabel>> label) => new() {
                 new HtmlString(Tags.TitleStart),
                 h.DisplayNameFor(label),
                 new HtmlString(Tags.TitleEnd),
@@ -30,5 +28,5 @@ public static class HtmlSelectItem
                 h.DropDownListFor(value, items, new { @class = Tags.FormControl } ),
                 h.ValidationMessageFor(value, string.Empty, new { @class = Tags.TextDanger }),
                 new HtmlString(Tags.DataEnd),
-        };
+         };
 }
